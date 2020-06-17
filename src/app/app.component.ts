6a0adf5cf509c9c396e5b0abce3c251ac256b778
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-17 18:28:14 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormArray, FormControl } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name.validator';
 import { passwordValidator } from './shared/password-validator';
 
@@ -8,21 +8,40 @@ import { passwordValidator } from './shared/password-validator';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
+ registrationForm: FormGroup;
 
 get userName(){
   return this.registrationForm.get('userName');
 }
+get email(){ //?
+  return this.registrationForm.get('email');
+}
+
+get alternateEmails(){
+  return this.registrationForm.get('alternateEmails') as FormArray;
+}
+
+addAlternateEmail(){
+  this.alternateEmails.push(this._fb.control(''))
+}
   constructor(private _fb : FormBuilder){}
-  registrationForm = this._fb.group({
+
+ngOnInit(){
+
+  this.registrationForm = this._fb.group({
     userName:['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/),forbiddenNameValidator(/admin/), forbiddenNameValidator(/username/), forbiddenNameValidator(/userName/)]],
     password:[''],
     confirmPassword:[''],
+    email: [''],
+    subscribe: [false],
     address: this._fb.group({
       city:[''],
       state: [''],
       postalCode: ['']
-    })
+    }),
+    alternateEmails:this._fb.array([])
     },{validator: passwordValidator});
     /* registrationForm = new FormGroup({
      userName: new FormControl('Sakura'),
@@ -34,6 +53,19 @@ get userName(){
        postalCode: new FormControl('')
      })
   })*/
+    this.registrationForm.get('subscribe').valueChanges
+    .subscribe(checkedValue =>{
+      const email=this.registrationForm.get('email');
+      if(checkedValue){
+        email.setValidators(Validators.required)
+      }else{
+        email.clearValidators();
+      }
+      email.updateValueAndValidity();
+    })
+}
+
+ 
 loadAPI(){
   this.registrationForm.patchValue({
     userName:'Alias',
